ccd7656e96db909428953ea87f96c9e32ee98007
Emre Öztürk | odc <emre.ozturk@odc.com.tr>
2017-07-06 10:41:05 +0300

    düzenlemeler yapıldı.

--- hesap.js
+++ hesap.js
@@ -2,19 +2,19 @@ var app = angular.module("myApp",['pascalprecht.translate']);
 app.config(["$translateProvider",function($translateProvider){
   
   var en_translations = {
-    "language" : "Calculator",
-    "num1" : "number 1",
-	"calc" : "calculate",
-	"num2" : "number 2",
-	"res" : "Result"
+    language : "Calculator",
+    num1 : "number 1",
+	calc : "calculate",
+	num2 : "number 2",
+	res : "Result"
   }
   
   var tr_translations = {
-    "language" : "Hesap makinesi",
-    "num1" : "carpan 1",
-	"calc" : "hesapla",
-	"num2" : "carpan 2",
-	"res" : "Sonuc"
+    language : "Hesap makinesi",
+    num1 : "carpan 1",
+	calc : "hesapla",
+	num2 : "carpan 2",
+	res : "Sonuc"
   }
   
   $translateProvider.translations('en',en_translations);
